0e872ea6e6478d99a66da9a63f3556c36c123ede
Liuwsh <eastwind10274@163.com>
2024-05-24 20:05:59 +0800

    feat: 拓展第二种图标

--- src/Funny_Json_Explorer.cpp
+++ src/Funny_Json_Explorer.cpp
@@ -1,20 +1,24 @@
 #include <iostream>
 #include <string>
 
-#include "JsonLoader/JsonLoader.h"
-#include "JsonRenderer/TreeRenderer.h"
-#include "JsonRenderer/RectRenderer.h"
 #include "JsonIcon/JsonIcon.h"
+#include "JsonRenderer/TreeRendererFactory.h"
+#include "JsonRenderer/RectRendererFactory.h"
+#include "JsonLoader/JsonLoader.h"
+
 int main()
 {
     JsonLoader loader;
-    JsonIcon icon("icon.config");
-    TreeRenderer tree_renderer(&icon);
-    RectRenderer rect_renderer(&icon);
     loader.load("test.json");
+
+    TreeRendererFactory tree_factory("icon.config");
+    RectRendererFactory rect_factory("icon_ext.config");
+    
     // loader.output("test_output.json");
-    std::string result = loader.draw(&rect_renderer);
-    // std::string result = loader.draw(&tree_renderer);
+    std::string result = loader.draw(&tree_factory);
+    std::cout << result;
+
+    result = loader.draw(&rect_factory);
     std::cout << result;
     
 }
